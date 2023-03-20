14c5888d8ff1263867a042dac049ba2551b52217
Anubhav Gupta <mail.anubhav06@gmail.com>
2023-03-20 18:46:19 +0530

    migrate to Twitter API v2
    Signed-off-by: Anubhav Gupta <mail.anubhav06@gmail.com>

--- twitterBot.py
+++ twitterBot.py
@@ -6,6 +6,7 @@ CONSUMER_KEY = config('CONSUMER_KEY')
 CONSUMER_SECRET = config('CONSUMER_SECRET')
 ACCESS_TOKEN = config('ACCESS_TOKEN')
 ACCESS_TOKEN_SECRET = config('ACCESS_TOKEN_SECRET')
+BEARER_TOKEN = config('BEARER_TOKEN')
 
 # ID of @devopswithkunal account
 BOT_ID = 1460548132870111233
@@ -13,60 +14,69 @@ BOT_ID = 1460548132870111233
 SLEEP_TIME = 120
 
 # Twitter authentication
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+# auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+# auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-api = tweepy.API(auth)
+client = tweepy.Client(
+    BEARER_TOKEN,
+    CONSUMER_KEY, CONSUMER_SECRET,
+    ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+)
 
 try:
-    api.verify_credentials()
+    client.get_user(id=BOT_ID)
     print('🟢Authentication OK')
 except:
     print('🔴Error in authentication')
 
 
-class StreamListener(tweepy.Stream):
+def retweeted(tweet):
+    if tweet.author_id == BOT_ID:
+        try:
+            if tweet.referenced_tweet.type == "retweet" and tweet.referenced_tweet.id == tweet.id:
+                return True
+        except:
+            return False
+    return False
 
-    def on_status(self, tweet):
-        print(f" Tweet by @{tweet.author.screen_name}")
+class StreamListener(tweepy.StreamingClient):
+
+    def on_connect(self):
+        print('Connected to stream. Listening to tweets')
+
+    def on_tweet(self, tweet):
+        print(f" Tweet by @{tweet.author_id}")
         # Check if the tweet's author is not us and if the tweet is not a reply and Check if tweet is not already retweeted.
-        if tweet.in_reply_to_status_id is None and int(tweet.author.id) != int(BOT_ID) and tweet.retweeted is False:
-            blocked_ids = api.get_blocked_ids(stringify_ids = True)
+        if tweet.in_reply_to_user_id is None and int(tweet.author_id) != int(BOT_ID) and not retweeted(tweet):
+            blocked_ids = client.get_blocked()
             # Check if the tweet author is not blocked by us
-            if str(tweet.author.id) not in blocked_ids:
+            if str(tweet.author_id) not in blocked_ids:
                 # If a blocked tweet was retweeted by someone else who is not blocked, then check the original tweet's ID
-                if hasattr(tweet, 'retweeted_status'):
-                    if str(tweet.retweeted_status.user.id) not in blocked_ids:
-                        try:                    
-                            print('Attempting retweet ........ 🟡')
-                            api.retweet(tweet.id)
-                            print('Tweet succesfully retweeted ✅')
-                            time.sleep(int(SLEEP_TIME))
-                        except Exception as err:
-                            print(err)
-                    else:
-                        print('🛑Blocked tweet by: ', tweet.author.screen_name)
-                # If the tweet is not retweeted by anyone else, any the root tweet's author is not blocked.
-                else:
-                    print('No attribute')
-                    try:                    
-                        print('Attempting retweet ........ 🟡')
-                        api.retweet(tweet.id)
-                        print('Tweet succesfully retweeted ✅')
-                        time.sleep(int(SLEEP_TIME))
-                    except Exception as err:
-                        print(err)
+                try:                    
+                    print('Attempting retweet ........ 🟡')
+                    client.retweet(tweet.id)
+                    print('Tweet succesfully retweeted ✅')
+                    time.sleep(int(SLEEP_TIME))
+                except Exception as err:
+                    print(err)
             else:
-                print('🛑Blocked tweet by: ', tweet.author.screen_name)
+                print('🛑Blocked tweet by: ', tweet.author_id)
                 time.sleep(int(SLEEP_TIME))
 
     def on_error(self, status):
         print(f"🔴 Error while retweeting: {status}")
 
+    def on_closed(self, response):
+        print(f'🔴 Stream closed! {response}')
+
 
 stream = StreamListener(
-  CONSUMER_KEY, CONSUMER_SECRET,
-  ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+    BEARER_TOKEN,
+    wait_on_rate_limit=True
 )
+
 # Look for tweets with the below mentioned words/tags only
-stream.filter(track=['#DevOpsWithKunal', "#devopswithkunal", "DevOpswithKunal", "#DevOpswithkunal", "#devopsWithKunal", "#Devopswithkunal"])
+stream.add_rules(add=tweepy.StreamRule(value="#DevOpsWithKunal OR #devopswithkunal OR #DevOpswithKunal OR #DevOpswithkunal OR \
+#devopsWithKunal OR #Devopswithkunal"))
+
+stream.filter(tweet_fields=["created_at", "in_reply_to_user_id", "author_id", "referenced_tweets"])
